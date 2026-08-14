b067ea43b829d52e201abe8c75c344fca9bca085
Alex135135 <jonaton1@yandex.ru>
2026-08-14 22:03:49 +0300

    add component

---
+++ src/data.ts/data.ts
@@ -0,0 +1,29 @@
+export const popularServices = [
+    {
+        id: 1,
+        name: "Экспресс-мойка",
+        description: "Быстрая и качественная мойка кузова с пеной и сушкой. Идеально для ежедневного ухода.",
+        price: "500 ₽",
+        duration: "30 мин",
+        icon: "/icons/wash.svg",
+        popular: true,
+    },
+    {
+        id: 2,
+        name: "Керамическое покрытие",
+        description: "Надёжная защита кузова на 6 месяцев. Гидрофобный эффект и зеркальный блеск.",
+        price: "3 500 ₽",
+        duration: "2 часа",
+        icon: "/icons/ceramic.svg",
+        popular: true,
+    },
+    {
+        id: 3,
+        name: "Химчистка салона",
+        description: "Глубокая очистка тканей и кожи. Удаление запахов и бактерий. Освежает интерьер.",
+        price: "1 500 ₽",
+        duration: "2 часа",
+        icon: "/icons/interior.svg",
+        popular: false,
+    },
+];
