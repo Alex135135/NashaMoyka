40ebb0941af25bd52cc551fbe26bb540bbb43a48
Alex135135 <jonaton1@yandex.ru>
2026-08-16 21:28:33 +0300

    create page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,14 @@
 import { WhyUs } from '@/components/WhyUs/WhyUs'
 import { Welcome } from '@/components/Welcome/Welcome'
 import { SpecialOffer } from '@/components/SpecialOffer/SpecialOffer'
-import { cardInfoWhyUs } from '@/data/data'
+import { PopularServices } from '@/components/PopularServices/PopularServices'
+import { cardInfoWhyUs, popularServices } from '@/data/data'
+
 export default function Home() {
   return (
     <>
       <Welcome />
+      <PopularServices services={popularServices} />
       <WhyUs
         title={'Почему выбирают нас?'}
         texts={cardInfoWhyUs}
